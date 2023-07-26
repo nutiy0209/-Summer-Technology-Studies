14860ec4563a708c8a49b48aaf34cab03d7e316e
Justin <Justin920209@gmail.com>
2023-07-26 14:49:15 +0800

    fuckyou/composer.json

---
+++ 0726/fuckyou/app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Cookie;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+}
--- 0726/fuckyou/routes/api.php
+++ 0726/fuckyou/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'auth'],function(){
+    Route::get('/',[AuthController::class,'me']);
+    Route::post('register',[AuthController::class,'register']);
+});
